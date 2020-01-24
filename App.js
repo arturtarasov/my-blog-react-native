8ae780411783f528f56e610eba77c735d2ca1bc1
arturtarasov <artur-tarasov@mail.ru>
2020-01-24 18:03:53 +0300

    create app

---
+++ App.js
@@ -0,0 +1,23 @@
+import { AppLoading } from 'expo';
+import React, { useState } from 'react';
+import { Text, View } from 'react-native';
+
+import { bootstrap } from './src/bootstrap';
+
+export default function App() {
+  const [isReady, setIsReady] = useState(false);
+  if (!isReady) {
+    return (
+      <AppLoading
+        startAsync={bootstrap}
+        onFinish={() => setIsReady(true)}
+        onError={err => console.log(err)}
+      />
+    );
+  }
+  return (
+    <View>
+      <Text>Open up App.js to start working on your app!</Text>
+    </View>
+  );
+}
